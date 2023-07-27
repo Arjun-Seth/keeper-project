1e18559c99f8853523d8e9b1b75fd6077e28f611
Arjun-Seth <arjun.drag007@gmail.com>
2023-07-28 03:11:40 +0530

    changes made in some files

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 import CreateArea from "./CreateArea";
 
 function App() {
+  const [notes, setNotes] = useState([])
+
+  function addNote(inputNote) {
+    setNotes((prevNote) => {
+      return [...prevNote, inputNote];
+    })
+    // console.log(notes);
+  }
+
+  function deleteNote(id) {
+    // console.log(id);
+    setNotes((prevNotes) => {
+      return prevNotes.filter((noteitem, index) => {
+        return id !== index;
+      })
+    })
+
+  }
+
   return (
     <div>
       <Header />
-      <CreateArea />
-      <Note key={1} title="Note title" content="Note content" />
+      <CreateArea onAdd={addNote} />
+      {notes.map((note, index) => {
+        return <Note key={index} id={index} title={note.title} content={note.content} onDelete={deleteNote} />
+      })
+      }
+
       <Footer />
     </div>
   );
--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,12 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 
-function CreateArea() {
+function CreateArea(props) {
+  const [inputText, setInputText] = useState({
+    title: "",
+    content: ""
+  });
+
+
+
+
+  function handleChange(event) {
+    const value = event.target.value
+    const name = event.target.name
+    // console.log(newValue);
+    // console.log(inputValue);
+    setInputText((prevInputs) => {
+      return {
+        ...prevInputs,
+        [name]: value
+      }
+      // if (name === "title") {
+      //   return {
+      //     title: value,
+      //     content: prevInputs.content
+      //   };
+      // }
+      // else if (name === "content") {
+      //   return {
+      //     title: prevInputs.title,
+      //     content: value
+      //   }
+      // }
+    })
+  }
+
+
+  function submitInputItem(event) {
+    props.onAdd(inputText);
+    setInputText({
+      title: "",
+      content: ""
+    });
+    event.preventDefault();
+  }
+
   return (
     <div>
       <form>
-        <input name="title" placeholder="Title" />
-        <textarea name="content" placeholder="Take a note..." rows="3" />
-        <button>Add</button>
+        <input onChange={handleChange} name="title" placeholder="Title" value={inputText.title} />
+        <textarea onChange={handleChange} name="content" placeholder="Take a note..." rows="3" value={inputText.content} />
+        <button onClick={submitInputItem}>Add</button>
       </form>
     </div>
   );
